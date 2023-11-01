7e7201c95f7ffc5f9bd766e6d2606b4926a00326
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-11-01 14:41:36 +0530

    GPS and Wifi added

---
+++ C-Codes/GPS.c
@@ -0,0 +1,77 @@
+
+void delay(unsigned int count) {
+    unsigned int i;
+    for (i = 0; i < count; i++) {
+        for (int j = 0; j < 1000; j++) {
+            // Delay loop
+        }
+    }
+}
+
+void UART1_Init() {
+    // Configure UART0 with baud rate 9600, 8 data bits, 1 stop bit, no parity
+    U1LCR = 0x83;  // 8 bits, no parity, 1 stop bit, DLAB = 1
+    U1DLL = 97;    // Set baud rate to 9600
+    U1LCR = 0x03;  // DLAB = 0
+}
+
+char UART1_ReadChar() {
+    while (!(U1LSR & 0x01));  // Wait until receive data is available
+    return U1RBR;             // Return received character
+}
+
+void UART1_SendChar(char ch) {
+    while (!(U1LSR & 0x20));  // Wait until the transmit buffer is empty
+    U1THR = ch;               // Send the character
+}
+
+void UART1_SendString(const char *str) {
+    while (*str) {
+        UART1_SendChar(*str++);
+    }
+}
+
+char main() {
+    char gps_data[128]; // Make sure the array is large enough to hold GPS data
+    char *gpgga_prefix = "$GPGGA,";
+
+    PINSEL1 = 0x00000005;  // Configure UART0 pins
+    UART1_Init();
+
+    while (1) {
+        char received_char = UART1_ReadChar();
+        if (received_char == '$') {
+            // Start of a new NMEA sentence
+            memset(gps_data, 0, sizeof(gps_data));
+            gps_data[0] = '$';
+            gps_data[1] = received_char;
+            int i = 2;
+
+            while (1) {
+                received_char = UART1_ReadChar();
+                gps_data[i++] = received_char;
+
+                // Check if the NMEA sentence is complete
+                if (i >= 7 && strncmp(gps_data + i - 7, "\r\n", 2) == 0) {
+                    gps_data[i] = '\0';
+                    break;
+                }
+            }
+
+            // Check if it's a GPGGA sentence
+            if (strncmp(gps_data, gpgga_prefix, strlen(gpgga_prefix)) == 0) {
+                // Parse the GPGGA sentence for GPS coordinates
+                // Extract latitude, longitude, and other relevant information
+                // For example:
+                char latitude[12];
+                char longitude[12];
+                if (sscanf(gps_data, "$GPGGA,%*d.%*d,%10[^,],%*c,%11[^,],", latitude, longitude) == 2) {
+                    // latitude and longitude contain the GPS coordinates
+                    // You can use these values as needed
+                }
+            }
+        }
+    }
+
+    return gps_data;
+}
---
+++ C-Codes/WiFi.c
@@ -0,0 +1,62 @@
+void delay(unsigned int count) {
+    unsigned int i;
+    for (i = 0; i < count; i++) {
+        for (int j = 0; j < 1000; j++) {
+            // Delay loop
+        }
+    }
+}
+
+void UART0_Init() {
+    // Configure UART0 with baud rate 115200, 8 data bits, 1 stop bit, no parity
+    U0LCR = 0x83;  // 8 bits, no parity, 1 stop bit, DLAB = 1
+    U0DLL = 97;    // Set baud rate to 115200
+    U0LCR = 0x03;  // DLAB = 0
+}
+
+void UART0_SendChar(char ch) {
+    while (!(U0LSR & 0x20));  // Wait until the transmit buffer is empty
+    U0THR = ch;               // Send the character
+}
+
+void UART0_SendString(const char *str) {
+    while (*str) {
+        UART0_SendChar(*str++);
+    }
+}
+
+int sendJson(float accX, float accY, float accZ, float gyX, float gyY, float gyZ, float vib, float temp) {
+    PINSEL0 = 0x00000005;  // Configure UART0 pins
+    UART0_Init();
+
+    // Initialize ESP8266
+    UART0_SendString("AT\r\n");
+    delay(1000); // Wait for the ESP8266 to respond
+    UART0_SendString("AT+CWJAP=\"vivo-1906\",\"Siddhesh@5449\"\r\n"); // Connect to your Wi-Fi
+    delay(5000); // Wait for the ESP8266 to connect to Wi-Fi
+
+    // JSON data
+    char json_data[128]; // Make sure the array is large enough to hold your JSON data
+    snprintf(json_data, sizeof(json_data), "{\"accX\":\"%f\",
+        \"accY\":\"%f\",
+        \"accZ\":\"%f\",
+        \"gyX\":\"%f\",
+        \"gyY\":\"%f\",
+        \"gyZ\":\"%f\",
+        \"temp\":\"%f\",
+        \"vib\":\"%f\",
+    }", accX, accY, accZ, gyX, gyY, gyZ, temp, vib);
+    
+    // Send JSON data to a server
+    UART0_SendString("AT+CIPSTART=\"TCP\",\"your_server_ip\",your_server_port\r\n");
+    delay(2000);
+    UART0_SendString("AT+CIPSEND=");
+    UART0_SendChar(strlen(json_data) + 2);
+    UART0_SendString("\r\n");
+    delay(1000);
+    UART0_SendString(json_data);
+    delay(2000);
+    UART0_SendString("AT+CIPCLOSE\r\n"); // Close the connection
+
+    return 0;
+}
