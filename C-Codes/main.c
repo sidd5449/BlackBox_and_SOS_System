b92863eb50c84e6a894c1e02c97896adfa2840e2
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-11-03 11:36:24 +0530

    updated GPS file

---
+++ C-Codes/main.c
@@ -0,0 +1,14 @@
+#include <lpc214x.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "GPS.c"
+#include "GSM.c"
+#include "IMU.c"
+#include "LM35.c"
+#include "WiFi.c"
+
+int main(){
+    
+
+}
